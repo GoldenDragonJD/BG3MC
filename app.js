35b14aaa353029eb019826293f5ad9304b96240a
Juan Delgado <lisbe2003@gmail.com>
2024-03-06 15:35:29 -0500

    started work on the home page.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const mongo = require('mongoose');
+const mongo = require("mongoose");
 
 const app = express();
 
-app.listen(12345);
+app.use("/public/css", express.static(__dirname + "/public/css"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+const listener = app.listen(process.env.PORT || 12345, () => {
+  console.log(listener.address().port);
+});
