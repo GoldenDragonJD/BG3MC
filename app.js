df4e48d4f6d6753ddb968d5724f9d2619df77293
Juan Delgado <lisbe2003@gmail.com>
2024-03-06 17:55:52 -0500

    Update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongo = require("mongoose");
+const sha256 = require("js-sha256");
 
 const app = express();
 
@@ -7,6 +8,13 @@ app.use("/public/css", express.static(__dirname + "/public/css"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/spin", (req, res) => {
+  res.json({
+    username: req.body.input_user,
+    password: sha256(req.body.input_pass),
+  });
 });
 
 const listener = app.listen(process.env.PORT || 12345, () => {
