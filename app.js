5799a4a75e569f085a8b58c676df72787b5bb52f
Juan Delgado <lisbe2003@gmail.com>
2024-03-06 19:08:57 -0500

    fixed button bug.

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ const sha256 = require("js-sha256");
 const app = express();
 
 app.use("/public/css", express.static(__dirname + "/public/css"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/spin", (req, res) => {
+  if (req.body.action === "log_in") {
+    console.log("Log In");
+  } else if (req.body.action === "sign_up") {
+    console.log("Sign Up");
+  }
   res.json({
-    username: req.body.input_user,
-    password: sha256(req.body.input_pass),
+    username: `${req.body.user}`,
+    password: `${sha256(req.body.pass)}`,
   });
 });
 
