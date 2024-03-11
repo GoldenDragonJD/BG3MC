14b0d3c801600d6eb9db2ec22a40956f36879c8f
Juan Delgado <lisbe2003@gmail.com>
2024-03-11 02:16:34 -0400

    Started work on character select menu.

---
+++ public/Scripts/toggleButtons.js
@@ -0,0 +1,11 @@
+function toggleButton(button) {
+  const buttons = document.querySelectorAll(".select-button");
+  buttons.forEach((element) => {
+    if (!element.classList.contains("select-character")) {
+      element.classList.add("select-character");
+      element.classList.remove("selected");
+    }
+  });
+  button.classList.remove("select-character");
+  button.classList.add("selected");
+}
